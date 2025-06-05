5a16f747efaffaf796f43b09d1ed36b6053142a1
adrianpm-sudo <adrianpm@example.com>
2025-06-05 13:58:14 -0500

    Tercer Commit: Se ha escrito un comentario

--- holaMundo.py
+++ holaMundo.py
@@ -1,3 +1,11 @@
+"""
+- git status
+- git add . or git add holamundo.py
+- git commit -m  "mensaje para el commit"
+- git git push -u origin main
+"""
+
+
 print("Hola Mundo!")
 
 for i in range(10):
